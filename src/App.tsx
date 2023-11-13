493a3f0ed95d253db7c84cbe436e1998cef1ac0b
Emilien-Valain <emilien.valain@gmail.com>
2023-11-13 11:05:02 +0100

    Add todo functionality to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,58 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+type Todo = {
+  name: string
+  done: boolean
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState<Todo[]>([])
+  const [newTodo, setNewTodo] = useState<string>('')
+
+  function addTodo() {
+    setTodos([...todos, { name: newTodo, done: false }])
+    setNewTodo('')
+  }
+
+  function toggleDone(index: number) {
+    setTodos(todos.map((todo, i) => i === index ? { ...todo, done: !todo.done } : todo))
+  }
+
+  function removeTodo(index: number) {
+    setTodos(todos.filter((_, i) => i !== index))
+  }
+
+  const unfinishedTodos = todos.filter(todo => !todo.done)
+  const finishedTodos = todos.filter(todo => todo.done)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div class="flex flex-col flex-auto">
+      <div class="flex flex-row">
+        <input value={newTodo} onChange={(e) => setNewTodo(e.target.value)} class="outline rounded-md focus:outline-none focus:ring focus:border-blue-500 "/>
+        <button onClick={addTodo}>Add Todo</button>
       </div>
-      <h1 class="text-red-400">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <h2>Todo</h2>
+      <ul>
+        {unfinishedTodos.map((todo, index) => (
+          <li key={index}>
+            <input type="checkbox" checked={todo.done} onChange={() => toggleDone(index)} />
+            {todo.name}
+            <button onClick={() => removeTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      <h2>Done</h2>
+      <ul>
+        {finishedTodos.map((todo, index) => (
+          <li key={index}>
+            <input type="checkbox" checked={todo.done} onChange={() => toggleDone(index)} />
+            {todo.name}
+            <button onClick={() => removeTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
