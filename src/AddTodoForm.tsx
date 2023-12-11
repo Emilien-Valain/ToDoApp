a4e481942ee73473b7bb4ba326b4c8e630019065
Emilien-Valain <emilien.valain@gmail.com>
2023-12-11 10:13:05 +0100

    datepicker added

--- src/AddTodoForm.tsx
+++ src/AddTodoForm.tsx
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+import { format } from 'date-fns';
 
 interface AddTodoFormProps {
   addTodo: (name: string, dueDate: Date) => void;
@@ -6,13 +9,17 @@ interface AddTodoFormProps {
 
 const AddTodoForm: React.FC<AddTodoFormProps> = ({ addTodo }) => {
   const [newTodo, setNewTodo] = useState('');
-  const [dueDate, setDueDate] = useState(''); // Nouvel état pour la date
+  const [dueDate, setDueDate] = useState(new Date()); // Nouvel état pour la date
+
+  // function onChange(nextValue: React.SetStateAction<Date>) {
+  //   setDueDate(nextValue);
+  // }
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     addTodo(newTodo, new Date(dueDate));
     setNewTodo('');
-    setDueDate('');
+    // setDueDate();
   };
 
   return (
@@ -23,12 +30,14 @@ const AddTodoForm: React.FC<AddTodoFormProps> = ({ addTodo }) => {
         className="flex-1 border border-gray-300 p-2 rounded-l"
         placeholder="Add a new task"
       />
-      <input
-        type="date"
-        value={dueDate}
-        onChange={e => setDueDate(e.target.value)}
-        className="flex-1 border border-gray-300 p-2 rounded-r"
+      <DatePicker 
+        className="flex-1 border border-gray-300 p-2 rounded-l"
+        dateFormat="dd/MM/yyyy"
+        selected={dueDate} 
+        onChange={(date: React.SetStateAction<Date>) => setDueDate(date)} 
+        minDate={new Date()}
       />
+      {/* <Calendar onChange={(value) => setDueDate(value as Date)} /> */}
       <button type="submit" className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-r">
         Add Todo
       </button>
